5f6bcb64f24748d31e1bafbc70433207f5f8b38b
Faith Wangari <njihiafaith@gmail.com>
2025-04-15 11:59:53 +0300

    Adds

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  function writeCards(namesArray, eventName) {
+    const messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
